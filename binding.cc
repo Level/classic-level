e99a0af5019c770e63291602e7e55e2b619b4bca
Vincent Weevers <mail@vincentweevers.nl>
2022-01-30 23:51:31 +0100

    Make empty range options and keys valid

--- binding.cc
+++ binding.cc
@@ -231,7 +231,7 @@ static std::string* RangeOption (napi_env env, napi_value opts, const char* name
   if (HasProperty(env, opts, name)) {
     napi_value value = GetProperty(env, opts, name);
 
-    if (StringOrBufferLength(env, value) > 0) {
+    if (StringOrBufferLength(env, value) >= 0) {
       LD_STRING_OR_BUFFER_TO_COPY(env, value, to);
       std::string* result = new std::string(toCh_, toSz_);
       delete [] toCh_;
@@ -244,7 +244,6 @@ static std::string* RangeOption (napi_env env, napi_value opts, const char* name
 
 /**
  * Converts an array containing Buffer or string keys to a vector.
- * Empty elements are skipped.
  */
 static std::vector<std::string>* KeyArray (napi_env env, napi_value arr) {
   uint32_t length;
@@ -257,7 +256,7 @@ static std::vector<std::string>* KeyArray (napi_env env, napi_value arr) {
       napi_value element;
 
       if (napi_get_element(env, arr, i, &element) == napi_ok &&
-          StringOrBufferLength(env, element) > 0) {
+          StringOrBufferLength(env, element) >= 0) {
         LD_STRING_OR_BUFFER_TO_COPY(env, element, to);
         result->emplace_back(toCh_, toSz_);
       }
