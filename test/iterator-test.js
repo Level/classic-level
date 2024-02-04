9ccbe87f6e0278bcbe9b5364e84380cf70994092
Vincent Weevers <mail@vincentweevers.nl>
2024-02-04 19:49:44 +0100

    Fix cache bug in `iterator.nextv()`
    Category: fix

--- test/iterator-test.js
+++ test/iterator-test.js
@@ -37,3 +37,37 @@ test('iterator optimized for seek', async function (t) {
   await ite.close()
   return db.close()
 })
+
+// TODO: move to abstract-level
+for (const slice of [false, true]) {
+  test(`nextv() after next() respects cache (slice=${slice})`, async function (t) {
+    const db = testCommon.factory()
+
+    await db.open()
+    await db.batch()
+      .put('a', 'value')
+      .put('b', 'value')
+      .put('c', 'value')
+      .put('d', 'value')
+      .write()
+
+    const it = db.iterator()
+
+    // Two calls are needed to populate the cache
+    t.same(await it.next(), ['a', 'value'], 'entry a ok')
+    t.same(await it.next(), ['b', 'value'], 'entry b ok')
+    t.is(it.cached, 2)
+
+    if (slice) {
+      t.same(await it.nextv(1), [['c', 'value']], 'entries ok (1)')
+      t.same(await it.nextv(1), [['d', 'value']], 'entries ok (2)')
+      t.same(await it.nextv(1), [], 'empty')
+    } else {
+      t.same(await it.nextv(10), [['c', 'value'], ['d', 'value']], 'entries ok')
+      t.same(await it.nextv(10), [], 'empty')
+    }
+
+    await it.close()
+    return db.close()
+  })
+}
